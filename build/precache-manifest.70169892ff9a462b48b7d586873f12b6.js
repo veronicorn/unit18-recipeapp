b805b4127d1e2eb805490a06464d2934aa044a68
veronicorn <vronicalee@gmail.com>
2019-01-02 21:12:44 -0500

    Modifies package.json for GH deploy.

---
+++ build/precache-manifest.70169892ff9a462b48b7d586873f12b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "008bc225292cd63242bc",
+    "url": "/unit18-recipeapp/static/js/runtime~main.008bc225.js"
+  },
+  {
+    "revision": "9884185a8a901081efbb",
+    "url": "/unit18-recipeapp/static/js/main.9884185a.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/unit18-recipeapp/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "9884185a8a901081efbb",
+    "url": "/unit18-recipeapp/static/css/main.4655c2c2.chunk.css"
+  },
+  {
+    "revision": "b2d4d3e8be3ae01b7c87ba3df1877b6a",
+    "url": "/unit18-recipeapp/index.html"
+  }
+];
